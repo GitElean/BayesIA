d042ef822830f478e09a6fb590ab510eb625485f
root <root@MSI>
2023-02-16 21:29:10 -0600

    clase nodo realizada

--- BayesIA/Redes.py
+++ BayesIA/Redes.py
@@ -1,3 +1,16 @@
+import networkx as nx
+
 def nodo():
     return "nodo hecho"
 
+def aristas():
+    return 0
+
+def red():
+    return ""
+
+def algoritmo():
+    return 0
+
+def query():
+    return 0
